e9a50a3f46e7e95a895fe31587f1fd8463a8c283
rajeshkarn79 <107257803+rajeshkarn79@users.noreply.github.com>
2023-10-27 17:47:41 +0530

    Update settings.py
    SMTP mail Change

--- djnago_authentication/shopping/settings.py
+++ djnago_authentication/shopping/settings.py
@@ -139,8 +139,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_FROM = 'noreply@admin.com'
-EMAIL_HOST_USER = 'wewinintern@gmail.com'
-EMAIL_HOST_PASSWORD = 'qfjzedartijrokin'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
